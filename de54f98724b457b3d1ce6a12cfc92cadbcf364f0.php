df2fae985f6ce840ae7dd193f561f141efed1c25
Fatahrizaa <91896435+Fatahrizaa@users.noreply.github.com>
2021-10-17 23:09:16 +0700

    Add files via upload
    Upload Pertama

---
+++ de54f98724b457b3d1ce6a12cfc92cadbcf364f0.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Profile</title>
+        <link rel="stylesheet" href="css/stylesheet.css">
+    </head>
+    <body>
+        <div class="profile">
+            <div class="wrapper_profile">
+                <div class="photo_name">
+                    <img src="img/Profile.jpg">
+                    <h1>Fatahriza Aulia Ramadhani</h1>
+                </div>
+                <div class="detail_profile">
+                    <h3>Tempat Tanggal Lahir</h3>
+                    <p>Gresik, 10 Desember 2001</p>
+                    <h3>Alamat</h3>
+                    <p>Kedamean, Gresik, Jawa Timur</p>
+                    <h3>No.Telp</h3>
+                    <p>087866592405</p>
+                    <h3>Email</h3>
+                    <p>fatahrizaaulia26428@gmail.com</p>
+                    <h3>Gender</h3>
+                    <p>Perempuan</p>
+                    <h3>Agama</h3>
+                    <p>Islam</p>
+                    <h3>Hobi</h3>
+                    <p>Travelling</p>
+                </div>
+            </div>
+        </div>
+        <div class="edukasi">
+            <h3>Riwayat Pendidikan</h3>
+            <div class="container">
+                <div class="edu_left">
+                    <h4>Sekolah Dasar</h4>
+                    <p>2007 - 2013</p>
+                    <p>SD Negeri 2 Kedamean Gresik</p>
+                </div>
+                <div class="edu_right">
+                    <h4>Sekolah Menengah Pertama</h4>
+                    <p>2013 - 2016</p>
+                    <p>SMP Negeri 1 Kedamean Gresik</p>
+                </div>
+                <div class="edu_left">
+                    <h4>Sekolah Menengah Akhir</h4>
+                    <p>2016 - 2019</p>
+                    <p>SMA Negeri 1 Cerme Gresik</p>
+                </div>
+                <div class="edu_right">
+                    <h4>Sarjana</h4>
+                    <p>2019-sekarang</p>
+                    <p>Pendidikan Teknologi Informasi, Universitas Negeri Surabaya</p>
+                </div>
+            </div>
+        </div>
+        <div class="organisasi">
+            <h3>Pengalaman Organisasi</h3>
+            <div class="container">
+                <div class="organisir">
+                    <h4>2016-2017</h4>
+                    <p>Wakil Ketua Palang Merah Remaja SMA Negeri 1 Cerme Gresik</p>
+                </div>
+                <div class="organisir">
+                    <h4>2016-2017</h4>
+                    <p>Anggota Al-Banjari SMA Negeri 1 Cerme Gresik</p>
+                </div>
+                <div class="organisir">
+                    <h4>2019-2020</h4>
+                    <p>Anggota Departemen Remitra Forum Mahasiswa Bidikmisi UNESA</p>
+                </div>
+                <div class="organisir">
+                    <h4>2021</h4>
+                    <p>Anggota Karang Taruna Maha Asih Katimoho Kedamean Gresik</p>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <h3>Sosial Media</h3>
+            <div class="listfooter">
+                <ul>
+                    <li>
+                        <img src="img/Facebook.png">
+                        <a href="https://facebook.com/fatahriza.aulia">Fatahriza Aulia</a>
+                    </li>
+                    <li>
+                        <img src="img/Instagram.png">
+                        <a href="https://www.instagram.com/fatahrizaar_">fatahrizaar_</a>
+                    </li>
+                    <li>
+                        <img src="img/Youtube.png">
+                        <a href="https://www.youtube.com/channel/UCVwKh9_P5PChiBS2Xazf9nA">Fatahriza Aulia</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html><?php /**PATH C:\xampp\htdocs\My-Profile\resources\views/Profil.blade.php ENDPATH**/ ?>
